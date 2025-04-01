6b4ab24265e6235ebe1d35d7e36dffa478242a7a
chertila <pashafokin2007@gmal.com>
2025-04-01 11:19:59 +0200

    At the current version, the data on protein-containing products is taken out of all the tables and summed up in "data.txt" file

---
+++ src/main/java/TableHelper.java
@@ -0,0 +1,54 @@
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+public class TableHelper {
+    public static Map<Integer, List<String>> getDataFromTable(String filename) throws IOException {
+        String fileLocation = "C:\\Users\\user\\Desktop\\CSP\\tables\\"+filename+".xlsx";
+        FileInputStream file = new FileInputStream(new File(fileLocation));
+        Workbook workbook = new XSSFWorkbook(file);
+        Sheet sheet = workbook.getSheetAt(2);
+
+        Map<Integer, List<String>> data = new HashMap<>();
+        int i = 0;
+        for (Row row : sheet) {
+            data.put(i, new ArrayList<String>());
+            Cell cell = row.getCell(0);
+            if(cell != null){
+                if (cell.getCellType().equals(CellType.STRING)) {
+                    if((cell.getRichStringCellValue().getString().contains("TOTAL") ||
+                            cell.getRichStringCellValue().getString().equals("QUESO") ||
+                            cell.getRichStringCellValue().getString().equals("YOGURT") ||
+                            cell.getRichStringCellValue().getString().toLowerCase().contains("kgs")) &&
+                            !cell.getRichStringCellValue().getString().contains("ALIMENTACION") &&
+                            !cell.getRichStringCellValue().getString().contains("TOTAL HUEVOS"))
+                    {
+                        data.get(new Integer(i)).add(cell.getRichStringCellValue().getString());
+                        boolean flag = false;
+                        if(cell.getRichStringCellValue().getString().equals("QUESO")){
+                            flag = true;
+                        }
+                        cell = row.getCell(1);
+                        if (DateUtil.isCellDateFormatted(cell)) {
+                            data.get(i).add(cell.getDateCellValue() + "");
+                        } else {
+                            data.get(i).add(cell.getNumericCellValue() + "");
+                        }
+                        if(flag){
+                            break;
+                        }
+                        ++i;
+                    }
+                }
+            }
+        }
+        return data;
+    }
+}
