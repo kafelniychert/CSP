d47aee578d3e683fc3f48b2445bde48fdda6b8e4
chertila <pashafokin2007@gmal.com>
2025-04-07 03:42:05 +0200

    Added auto - generation of a simple percentage and minimum - maximum based analysis of changes that occurred with the consumption of the product

--- src/main/java/GraphHelper.java
+++ src/main/java/GraphHelper.java
@@ -1,9 +1,13 @@
 import com.github.sh0nk.matplotlib4j.Plot;
 import com.github.sh0nk.matplotlib4j.PythonExecutionException;
+
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class GraphHelper {
     /**
@@ -51,23 +55,81 @@ public class GraphHelper {
     }
 
     /**
-     * A method that adds a normal, line style graph onto a provided Plot object of the mass consumed for a specified product type throughout all the years of the data set
+     * A method that adds a normal, line style graph onto a provided Plot object of the mass consumed for a specified product type throughout all the years of the data set.
+     * It also writes a simple percentage and minimum - maximum based analysis of changes that occurred with the consumption of the product
      * @param plt The Plot object onto which the graph is added.
      * @param arrayList The list with data to be graphed.
      * @param index The needed product type.
      * @param color The desired color of the graph.
      * @param style the type of line (-, .-, etc)
+     * @param file The .txt file into which the analysis is written for the product type
      * @return Returns the provided Plot objects with the graph added
      */
-    public static Plot graphProduct(Plot plt, ArrayList<Map<Integer, List<String>>> arrayList, int index, String color, String style) throws PythonExecutionException, IOException {
+    public static Plot graphProduct(Plot plt, ArrayList<Map<Integer, List<String>>> arrayList, int index, String color, String style, File file) throws PythonExecutionException, IOException {
         ArrayList<Integer> x = new ArrayList<>();
         for (int i = 1999; i < 2024; ++i) {
             x.add(i);
         }
         ArrayList<Double> y = new ArrayList<>();
+        int maxIndex = 0;
+        int minIndex = 0;
         for (int i = 0; i < 25; ++i) {
             y.add(Double.valueOf(arrayList.get(i).get(index).get(1)));
+            if(Double.valueOf(arrayList.get(i).get(index).get(1)) > Double.valueOf(arrayList.get(maxIndex).get(index).get(1))){
+                maxIndex = i;
+            }
+            if(Double.valueOf(arrayList.get(i).get(index).get(1)) < Double.valueOf(arrayList.get(minIndex).get(index).get(1))){
+                minIndex = i;
+            }
         }
+
+        Scanner scanner = new Scanner(file);
+        String tmp = "";
+        while (scanner.hasNext()){
+            tmp += scanner.nextLine() + "\n";
+        }
+        FileWriter fileWriter = new FileWriter("C:\\Users\\user\\Desktop\\CSP\\data.txt");
+        String point1 = "(maximum)";
+        String point2 = "(minimum)";
+        if(maxIndex > minIndex){
+            int tmp2 = minIndex;
+            minIndex = maxIndex;
+            maxIndex = tmp2;
+            String tmp3 = point1;
+            point1 = point2;
+            point2 = tmp3;
+        }
+        String analysis = arrayList.get(0).get(index).get(0) + "\n";
+        if(maxIndex != 0 && maxIndex != 24){
+            analysis += "The yearly consumption changed by " + Math.round((y.get(maxIndex) - y.get(0)) / y.get(0) * 100.0 * 100.0) / 100.0 + "% to reach the extreme point" +
+                    point1 + " of " + Math.round(y.get(maxIndex) * 100.0) / 100.0 + " tonnes in " + (1999 + maxIndex);
+            if(minIndex != 0 && minIndex != 24){
+                analysis += ". Next it changed by " + Math.round((y.get(minIndex) - y.get(maxIndex)) / y.get(maxIndex) * 100.0 * 100.0) / 100.0 + "% to reach the next extreme point" +
+                        point2 + " of " + Math.round(y.get(minIndex) * 100.0) / 100.0 + " tonnes in " + (minIndex + 1999);
+                analysis += ". Finally, by 2023 it changed by " + Math.round((y.get(24) - y.get(minIndex)) / y.get(minIndex) * 100.0 * 100.0) / 100.0 + "% to reach " +
+                        Math.round(y.get(24) * 100.0) / 100.0 + " tonnes.";
+            }
+            else{
+                analysis += ". Finally, by 2023 it changed by " + Math.round((y.get(24) - y.get(maxIndex)) / y.get(maxIndex) * 100.0 * 100.0) / 100.0 + "% to reach " +
+                        Math.round(y.get(24) * 100.0) / 100.0 + " tonnes.";
+            }
+        }
+        else{
+            if(minIndex != 0 && minIndex != 24){
+                analysis += "The yearly consumption changed by " + Math.round((y.get(minIndex) - y.get(0)) / y.get(0) * 100.0 * 100.0) / 100.0 + "% to reach the extreme point" +
+                        point2 + " of " + Math.round(y.get(minIndex) * 100.0) / 100.0 + " tonnes in " + (minIndex + 1999);
+                analysis += ". Finally, by 2023 it changed by " + Math.round((y.get(24) - y.get(minIndex)) / y.get(minIndex) * 100.0 * 100.0) / 100.0 + "% to reach " +
+                        Math.round(y.get(24) * 100.0) / 100.0 + " tonnes.";
+            }
+            else{
+                analysis += "The yearly consumption changed by " + Math.round((y.get(24) - y.get(0)) / y.get(0) * 100.0 * 100.0) / 100.0 + "% to reach the extreme point" +
+                        point2 + " of " + Math.round(y.get(minIndex) * 100.0) / 100.0 + " tonnes from 1999 to 2023";
+            }
+        }
+
+        fileWriter.write(tmp + "\n" + analysis);
+        fileWriter.close();
+        scanner.close();
 
         plt.plot().color(color).add(x, y).label(arrayList.get(0).get(index).get(0)).linestyle(style);
         plt.xlabel("Year");
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import com.github.sh0nk.matplotlib4j.Plot;
 import com.github.sh0nk.matplotlib4j.PythonConfig;
 import com.github.sh0nk.matplotlib4j.PythonExecutionException;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
@@ -11,6 +13,14 @@ public class Main {
             arrayList.add(TableHelper.getDataFromTable(String.valueOf(i+1999)));
         }//Extracting all 24 excel tables into the Array List
 
+        File file = new File("C:\\Users\\user\\Desktop\\CSP\\data.txt");
+        if(!file.exists()) {
+            file.createNewFile();
+        }
+        else{
+            file.delete();
+            file.createNewFile();
+        }
         arrayList.add(TableHelper.calculateTotal(arrayList)); //adding the total for each product as the 26th element
         Plot plt = Plot.create(PythonConfig.pythonBinPathConfig("C:\\Users\\user\\AppData\\Local\\Programs\\Python\\Python313\\python.exe"));
         plt = GraphHelper.graphColumns(plt, arrayList, 25, "000000", "-", "Total of each by type");
@@ -19,7 +29,7 @@ public class Main {
         plt.executeSilently();
         for (int i = 0; i < 7; ++i) {
             Plot plt2 = Plot.create(PythonConfig.pythonBinPathConfig("C:\\Users\\user\\AppData\\Local\\Programs\\Python\\Python313\\python.exe"));
-            GraphHelper.graphProduct(plt2, arrayList, i, "000000", "-");
+            GraphHelper.graphProduct(plt2, arrayList, i, "000000", "-", file);
             plt2.legend();
             plt2.savefig("C:/Users/user/Desktop/CSP/graphs/" + arrayList.get(0).get(i).get(0) + ".png").dpi(200);
             plt2.executeSilently();
